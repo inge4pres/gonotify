ece4ffaf8cf5439f798f0df8885d7cb7c3a4ef03
francesco gualazzi <fgualazzi@gmail.com>
2015-05-06 13:49:23 +0200

    Adding users

--- gnbackend/gnusers.go
+++ gnbackend/gnusers.go
@@ -1,6 +1,19 @@
 package gnbackend
 
 type User struct {
-	Id                 uint
+	Id                 int64
 	Uname, Rname, Mail string
+}
+
+func NewUser() *User {
+	return &User{
+		Id:    -1,
+		Uname: "",
+		Rname: "",
+		Mail:  "",
+	}
+}
+
+func Register(name, rname, mail, pwd string) (int64, error) {
+	return -1, nil
 }
